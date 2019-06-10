f91fde48aa62e7a5292b4888d114b5d193af11c1
Jakub <dabrowski.kuba@gmail.com>
2019-06-10 17:30:20 +0200

    template folder fix

--- iamalive/web/__init__.py
+++ iamalive/web/__init__.py
@@ -1,9 +1,11 @@
-
+import os
 global web_app
+
 
 def create_web(app):
     global web_app
-    app.template_folder = 'web/templates'
-    app.static_folder = 'web/static'
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    app.template_folder = os.path.join(dir_path, 'templates')
+    app.static_folder = os.path.join(dir_path, 'static')
     web_app = app
     from iamalive.web import views
